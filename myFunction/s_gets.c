473bb99b63cebad7b783cf8771890c2f8a5545eb
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-12 14:56:40 +0800

    Create s_gets.c

---
+++ myFunction/s_gets.c
@@ -0,0 +1,22 @@
+/*自用gets函数
+ * 读取整行输入，并用空字符代替换行符
+ * 或者读取一部分输入，并丢弃剩余字符*/
+
+#include <stdio.h>
+char * s_gets(char Buffer[], int maxCount){
+    char * ret_value;
+    int i = 0;
+    ret_value = fgets(Buffer, maxCount, stdin);
+    if (ret_value){
+        //替换'\n'为'\0'
+        while (Buffer[i] != '\n' && Buffer[i] != '\0') {
+            i++;
+        }
+        if (Buffer[i] == '\n')
+            Buffer[i] = '\0';
+        else{
+            while (getchar() != '\n');
+        }
+    }
+    return ret_value;
+}
