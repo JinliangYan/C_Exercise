8cf9fbf36c71b6df4dcbc5d9d9f601212d3d354f
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-12 23:05:14 +0800

    Update s_gets.c

--- myFunction/s_gets.c
+++ myFunction/s_gets.c
@@ -3,6 +3,7 @@
  * 或者读取一部分输入，并丢弃剩余字符*/
 
 #include <stdio.h>
+//数组表示法
 char * s_gets(char Buffer[], int maxCount){
     char * ret_value;
     int i = 0;
@@ -14,6 +15,23 @@ char * s_gets(char Buffer[], int maxCount){
         }
         if (Buffer[i] == '\n')
             Buffer[i] = '\0';
+        else{
+            while (getchar() != '\n');
+        }
+    }
+    return ret_value;
+}
+
+//指针表示法
+char * s_gets(char * Buffer, int maxCount){
+    char * ret_value;
+    ret_value = fgets(Buffer, maxCount, stdin);
+    if (ret_value){
+        //替换'\n'为'\0'
+        while (*Buffer != '\n' && *Buffer != '\0')
+            Buffer++;
+        if (*Buffer == '\n')
+            *Buffer = '\0';
         else{
             while (getchar() != '\n');
         }
