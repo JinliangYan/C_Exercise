a8bfb783bfd4cd7f980e3047b8157b9ed497566c
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-12 23:35:45 +0800

    Update 1.c

--- C_Primer_Plus_Exercise/Chapter11/11.13/1.c
+++ C_Primer_Plus_Exercise/Chapter11/11.13/1.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #define STLEN 15
 char * get_str(char str[], int n);
 
 int main(void){
-    char str[1];
+    char str[STLEN + 1];
     get_str(str, STLEN);
     fputs(str, stdout);
+    printf("\n\n%llu", strlen(str));
     return 0;
 }
 
 char * get_str(char str[], int n){
     char ch;
     char * ret_value = str;
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n; i++) {
         *str++ = getchar();
     }
     *str = '\0';
