299f8aabfbd735327fdf7add367e1253dba76c8c
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-12 23:30:45 +0800

    Update 1.c

--- C_Primer_Plus_Exercise/Chapter11/11.13/1.c
+++ C_Primer_Plus_Exercise/Chapter11/11.13/1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#define STLEN 15
+char * get_str(char str[], int n);
 
+int main(void){
+    char str[1];
+    get_str(str, STLEN);
+    fputs(str, stdout);
+    return 0;
+}
+
+char * get_str(char str[], int n){
+    char ch;
+    char * ret_value = str;
+    for (int i = 0; i < n - 1; i++) {
+        *str++ = getchar();
+    }
+    *str = '\0';
+    return ret_value;
+}
