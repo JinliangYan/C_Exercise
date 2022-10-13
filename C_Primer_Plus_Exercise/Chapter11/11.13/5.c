42fd85bcf29c3b4c2d7f89350573bb5222e0aeb0
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-13 11:27:59 +0800

    Create 5.c

---
+++ C_Primer_Plus_Exercise/Chapter11/11.13/5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define STLEN 100
+
+char * mystrchr(char *str, char ch);
+char * s_gets(char * Buffer, int maxCount);
+int main(void){
+    char str[STLEN];
+    char search_ch;
+    puts("Please enter a string(EOF to exit):");
+    while (s_gets(str, STLEN) != NULL){
+        printf("Please enter a char you want to search from the string(EOF to exit):");
+        search_ch = getchar();
+        while (getchar() != '\n')
+            continue;
+        puts("String:");
+        puts(str);
+        if (mystrchr(str, search_ch) == NULL)
+            printf("Not exist %c in the string.\n", search_ch);
+        else
+            printf("Exist %c in the string.\n", search_ch);
+        puts("You can enter a string(EOF to exit):");
+    }
+    return 0;
+}
+
+char * mystrchr(char *str, char ch){
+    while (*str){
+        if (ch == *str++ && *str) {
+            return str - 1;
+        }
+    }
+    return NULL;
+}
+
+char * s_gets(char * Buffer, int maxCount){
+    char * ret_value;
+    ret_value = fgets(Buffer, maxCount, stdin);
+    if (ret_value){
+        //替换'\n'为'\0'
+        while (*Buffer != '\n' && *Buffer != '\0')
+            Buffer++;
+        if (*Buffer == '\n')
+            *Buffer = '\0';
+        else{
+            while (getchar() != '\n');
+        }
+    }
+    return ret_value;
+}
