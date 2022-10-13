ae9225caf533cea352bc36497f54f7a6fe3e26b8
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-13 17:27:04 +0800

    Create 10.c

---
+++ C_Primer_Plus_Exercise/Chapter11/11.13/10.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+#define STLEN 50
+
+char * s_gets(char * Buffer, int maxCount);
+void del_str_space(char * str);
+int main(void)
+{
+    char str[STLEN];
+    puts("Please enter a string,I will delete all space(empty line or EOF to exit):");
+    while (s_gets(str, STLEN))
+    {
+        del_str_space(str);
+        puts("Done!");
+        puts(str);
+        puts("--------------------------------------------------------------------------------");
+        puts("Please enter a string,I will delete all space(empty line or EOF to exit):");
+    }
+    puts("Bye");
+    return 0;
+}
+
+void del_str_space(char * str)
+{
+    char tmp[STLEN];
+    char * ptmp = tmp;
+    char * pstr = str;
+    while (*pstr)
+    {
+        if (! isspace(*pstr))
+            *ptmp++ = *pstr++;
+        else
+            pstr++;
+    }
+    ptmp = tmp;
+    while (*ptmp)
+    {
+        *str++ = *ptmp++;
+    }
+    *str = '\0';
+}
+
+char * s_gets(char * Buffer, int maxCount)
+{
+    char * ret_value;
+    ret_value = fgets(Buffer, maxCount, stdin);
+    if (Buffer[0] == '\n')
+        ret_value = NULL;
+    if (ret_value){
+        //替换'\n'为'\0'
+        while (*Buffer != '\n' && *Buffer != '\0')
+            Buffer++;
+        if (*Buffer == '\n')
+            *Buffer = '\0';
+        else{
+            while (getchar() != '\n');
+        }
+    }
+    return ret_value;
+}
