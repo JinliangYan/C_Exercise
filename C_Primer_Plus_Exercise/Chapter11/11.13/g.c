8bb5773164cbe46ade0c5256d8462375198c58bf
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-14 23:25:28 +0800

    Create g.c

---
+++ C_Primer_Plus_Exercise/Chapter11/11.13/g.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+int my_atoi(char str[]);
+int main(void){
+    int a = my_atoi("123");
+    printf("%d", a);
+    return 0;
+}
+
+int my_atoi(char str[]){
+    int integer = 0;
+    int ct = 0;
+    int point_num = 0;
+    bool is_negative = *str == '-' ? true : false;
+    /*跳过前面所有0，
+     * 如果直到字符串结尾，则返回0↓*/
+    while (*str++ == '0'){
+        if (*str == '\0')
+            return 0;
+    }
+/*检查是否有非数字，
+ * 小数点只能有一个,
+ * 并且记录数字个数↓*/
+    while (str[ct]){
+        if (str[ct] == '.'){
+            str++;
+            point_num++;
+        }
+        if (!isdigit(str[ct])|| point_num > 1){
+            return 0;
+        }
+        else{
+            if (point_num < 1)
+                ct++;
+        }
+    }
+    /*前面为了跳过小数点，指针+1，
+     * 现在-1复位*/
+    --str;
+    while (ct > 0){
+        integer += (str[ct - 1] - '0') * (int)pow(10, ct);
+        ct--;
+    }
+    if (is_negative)
+        integer = -integer;
+    return integer;
+}
