651aef86e1d66112ed6f03ae8ee611601e619361
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-14 15:53:40 +0800

    Create 9.c

---
+++ experiment/experiment1/9.c
@@ -0,0 +1,32 @@
+/*111： 憇和余慖
+
+
+
+题目描述
+
+输入任意两庆整慖,输慩它们的憇慖和余慖。
+
+输入
+
+任意两庆整慖
+
+输慩
+
+憇慖 和 余慖
+
+
+
+慨例输入
+
+5 3
+
+慨例输慩
+
+Quotient=1,Remainder=2*/
+#include <stdio.h>
+int main(void){
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("Quotient=%d,Remainder=%d", a / b, a % b);
+    return 0;
+}
