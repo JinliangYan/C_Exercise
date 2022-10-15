369c5db1e7c440f004fa8c7a502e86a8fdb15b64
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-15 13:38:01 +0800

    Update g.c

--- C_Primer_Plus_Exercise/Chapter11/11.13/g.c
+++ C_Primer_Plus_Exercise/Chapter11/11.13/g.c
@@ -5,45 +5,49 @@
 
 int my_atoi(char str[]);
 int main(void){
-    int a = my_atoi("3");
+    int a = my_atoi("-000001281.8");
     printf("%d", a);
     return 0;
 }
 
 int my_atoi(char str[]){
-    int integer = 0;
-    int ct = 0;
-    int point_num = 0;
+    int integer = 0, point_num = 0, i = 0;
+    int tmp;
+    char * str_start = str;
     bool is_negative = *str == '-' ? true : false;
+    if (is_negative)
+        str_start = ++str;
     /*跳过前面所有0，
      * 如果直到字符串结尾，则返回0↓*/
-    while (*str++ == '0'){
+    while (*str == '0'){
         if (*str == '\0')
             return 0;
+        str++;
     }
-    str--;
 /*检查是否有非数字，
  * 小数点只能有一个,
- * 并且记录数字个数↓*/
-    while (str[ct]){
-        if (str[ct] == '.'){
-            str++; //           NUGNUGUNOKNFoak' sdgoisdp 我日 bug原因 在这！！！！！！！！！！！！！！！！！！！！！！
+ * 若不满足，返回0↓*/
+    while (*str){
+        if (*str == '.'){
+            str++;
             point_num++;
         }
-        else if (!isdigit(str[ct])|| point_num > 1){
+        else if (!isdigit(*str)|| point_num > 1)
             return 0;
-        }
-        else{
-            if (point_num < 1)
-                ++ct;
-        }
+        else
+            str++;
     }
-    /*前面为了跳过小数点，指针+1，
-     * 现在-1复位*/
-    --str;
-    while (ct > 0){
-        integer += (str[ct - 1] - '0') * (int)pow(10, ct);
-        ct--;
+    str--;
+/*如果有小数点，
+ * 则把指针前进至小数点前一位，
+ * 便于后面计算整数*/
+    if (point_num)
+        while (*str-- != '.')
+            ;
+
+    while (str >= str_start) {
+        tmp = *str-- - '0';
+        integer += tmp * (int)pow(10, i++);
     }
     if (is_negative)
         integer = -integer;
