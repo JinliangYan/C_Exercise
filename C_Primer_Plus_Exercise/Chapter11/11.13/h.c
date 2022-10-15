f3015036ed6e3d52c9f846ce2cb6464c31da4d41
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-15 15:01:46 +0800

    Create h.c

---
+++ C_Primer_Plus_Exercise/Chapter11/11.13/h.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAX 1024
+#define PRT_ORIG "-p"
+#define PRT_UPPER "-u"
+#define PRT_LOWER "-l"
+
+char * s_gets(char * Buffer, int maxCount);
+void usr_input(char *str, int max_count);
+void operate(char **command, char *text);
+int main(int argc, char *argv[]){
+    if (argc > 2){
+        puts("Too many parameters were entered.");
+        return 0;
+    }
+    char text[MAX + 1];
+    usr_input(text, MAX);
+    operate(argv, text);
+    return 0;
+}
+
+void usr_input(char *str, int max_count){
+    puts("Please enter the text (EOF or blank line to end your entry):");
+    int ch, is_blank_line, ct = 0;
+    while ((ch = getchar()) != EOF || ct < max_count) {
+        if (ch == '\n') {
+            if ((is_blank_line = getchar()) == '\n')
+                break;
+            str[ct++] = (char)ch;
+            str[ct++] = (char)is_blank_line;
+        } else{
+            str[ct++] = (char)ch;
+        }
+    }
+    str[ct] = '\0';
+}
+
+void operate(char **command, char *text){
+    char *start = text;
+    if (command[1] == NULL || !strcmp(PRT_ORIG, command[1])){
+        ;
+    } else if (!strcmp(PRT_UPPER, command[1])){
+        while (*text){
+            if (isalpha(*text)){
+                *text = toupper(*text);
+                text++;
+                continue;
+            }
+            text++;
+        }
+    } else if (!strcmp(PRT_LOWER, command[1])) {
+        while (*text){
+            if (isalpha(*text)){
+                *text = toupper(*text);
+                text++;
+                continue;
+            }
+            text++;
+        }
+    } else{
+        puts("ERROR!");
+        return;
+    }
+    puts(start);
+}
+
+
