664ab4ad7f3316aede6790ff3bfe81742995204a
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-17 23:02:31 +0800

    Create pe12-2a.h

---
+++ C_Primer_Plus_Exercise/Chapter12/2/pe12-2a.h
@@ -0,0 +1,5 @@
+#define US 1
+#define METRIC 0
+void set_mode(int *mode);
+void get_info(int mode, double *distance, double *fuel_consume);
+void show_info(int mode, double distance, double fuel_consume);
