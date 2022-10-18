732fd1263a46e0fccf2f1708d641b44f21c8e7d8
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-18 14:02:55 +0800

    Update 4.c

--- C_Primer_Plus_Exercise/Chapter12/4.c
+++ C_Primer_Plus_Exercise/Chapter12/4.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#define NUM 100
 
 int main(void){
-    int rand_num[10];
+    int rand_num[NUM];
     time_t time1 = time(&time1);
     srand((unsigned int )time1);
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < NUM; i++) {
         rand_num[i] = rand() % 10 + 1;
     }
 
     int tmp;
-    for (int i = 0; i < 9; i++) {
-        for (int j = i + 1; j < 10; j++) {
+    for (int i = 0; i < NUM - 1; i++) {
+        for (int j = i + 1; j < NUM; j++) {
             if (rand_num[i] < rand_num[j]){
                 tmp = rand_num[i];
                 rand_num[i] = rand_num[j];
@@ -21,7 +22,7 @@ int main(void){
         }
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < NUM; i++) {
         printf("%d ", rand_num[i]);
     }
     return 0;
