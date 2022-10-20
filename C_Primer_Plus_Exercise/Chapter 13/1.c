8e986ba398f302eb478f923859546c1a74d541dc
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-20 22:18:28 +0800

    Update 1.c

--- C_Primer_Plus_Exercise/Chapter 13/1.c
+++ C_Primer_Plus_Exercise/Chapter 13/1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SLEN  260
 
+int main(void) {
+    int ch;
+    FILE *fp;
+    char file_name[SLEN];
+    char BUFFER[3000];
+    unsigned long count = 0;
+    printf("Please enter the file name: ");
+    scanf("%s", file_name);
+    if ((fp = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "Failed to open file.");
+        exit(EXIT_FAILURE);
+    }
+    while ((ch = getc(fp)) != EOF) {
+        putc(ch, stdout);
+        count++;
+    }
+    fclose(fp);
+    printf("File %s has %lu characters.\n", file_name, count);
+    return 0;
+}
