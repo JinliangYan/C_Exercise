add56bb9fa472d8768b5b491fdf7b2aa0b7c40e1
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-22 09:31:39 +0800

    Update b.c

--- C_Primer_Plus_Exercise/Chapter 13/7/b.c
+++ C_Primer_Plus_Exercise/Chapter 13/7/b.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+    FILE *f1, *f2;
+    int ch1, ch2;
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s file1 file2", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if ((f1 = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    if ((f2 = fopen(argv[2], "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+    ch1 = getc(f1);
+    ch2 = getc(f2);
+    while (ch1 != EOF || ch2 != EOF) {
+        while (ch1 != EOF) {
+            putchar(ch1);
+            ch1 = getc(f1);
+            if (ch1 == '\n') {
+//                putchar('\n');
+                ch1 = getc(f1);
+                break;
+            }
+        }
+        while (ch2 != EOF) {
+            putchar(ch2);
+            ch2 = getc(f2);
+            if (ch2 == '\n') {
+                putchar('\n');
+                ch2 = getc(f2);
+                break;
+            }
+        }
+    }
+    return 0;
+}
