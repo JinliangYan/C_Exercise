6b2c4d5276235429f9e5331d7dd7e1d4f2bc45dd
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-22 11:52:31 +0800

    Update 8.c

--- C_Primer_Plus_Exercise/Chapter 13/8.c
+++ C_Primer_Plus_Exercise/Chapter 13/8.c
@@ -2,33 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+void show_info(char seek, char *file_name);
 int main(int argc, char *argv[]) {
-    if (argc < 2 || argc > 3 || strlen(argv[1]) != 1) {
+    if (argc < 2 || strlen(argv[1]) != 1) {
         fprintf(stderr, "Usage: %s a_character [file]", argv[0]);
         exit(EXIT_FAILURE);
     }
     int ch, ct = 0;
     char seek = argv[1][0];
     int status;
-    FILE *fp;
     /*0表示程序从标准输入读取，
      * 1表示从文件读取↓*/
     status = argc == 2 ? 0 : 1;
     if (status) {
-        if ((fp = fopen(argv[2], "r")) == NULL) {
-            fprintf(stderr, "Failed to open %s", argv[2]);
-            exit(EXIT_FAILURE);
+        for (int i = 2; i < argc; i++) {
+            show_info(seek, argv[i]);
         }
-        ch = getc(fp);
     } else {
         ch = getchar();
+        while (ch != EOF) {
+            if (ch == seek) {
+                ct++;
+            }
+            ch = getc(stdin);
+        }
+        printf("There are %d %c in your entry.\n", ct, seek);
     }
+}
 
+void show_info(char seek, char *file_name) {
+    FILE *fp;
+    int ch;
+    int ct = 0;
+    if ((fp = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s", file_name);
+        exit(EXIT_FAILURE);
+    }
+    ch = getc(fp);
     while (ch != EOF) {
         if (ch == seek) {
             ct++;
         }
-        ch = status ? getc(fp) : getc(stdin);
+        ch = getc(fp);
     }
-    printf("There are %d %c.", ct, seek);
+    printf("There are %d %c in %s.\n", ct, seek, file_name);
 }
