f1a3fe13e9d26b38bcaaba25c00017d8ccf275a4
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-22 17:23:53 +0800

    Create c.c

---
+++ C_Primer_Plus_Exercise/Chapter 13/c.c
@@ -0,0 +1,85 @@
+//text文本文件
+/*
+0 0 9 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 2 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 9 0 0 0 0 0 0 0 5 8 9 9 8 5 5 2 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 1 9 8 5 4 5 2 0 0 0 0 0 0 0 0 0
+0 0 0 0 9 0 0 0 0 0 0 0 5 8 9 9 8 5 0 4 5 2 0 0 0 0 0 0 0 0
+0 0 9 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 4 5 2 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 1 8 5 0 0 0 4 5 2 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 4 5 2 0 0 0 0 0
+5 5 5 5 5 5 5 5 5 5 5 5 5 8 9 9 8 5 5 5 5 5 5 5 5 5 5 5 5 5
+8 8 8 8 8 8 8 8 8 8 8 8 5 8 9 9 8 5 8 8 8 8 8 8 8 8 8 8 8 8
+9 9 9 9 0 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 3 9 9 9 9 9 9 9
+8 8 8 8 8 8 8 8 8 8 8 8 5 8 9 9 8 5 8 8 8 8 8 8 8 8 8 8 8 8
+5 5 5 5 5 5 5 5 5 5 5 5 5 8 9 9 8 5 5 5 5 5 5 5 5 5 5 5 5 5
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 6 6 0 0 0 0 0 0
+0 0 0 0 2 2 0 0 0 0 0 0 5 8 9 9 8 5 0 0 5 6 0 0 6 5 0 0 0 0
+0 0 0 0 3 3 0 0 0 0 0 0 5 8 9 9 8 5 0 5 6 1 1 1 1 6 5 0 0 0
+0 0 0 0 4 4 0 0 0 0 0 0 5 8 9 9 8 5 0 0 5 6 0 0 6 5 0 0 0 0
+0 0 0 0 5 5 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 6 6 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 5 8 9 9 8 5 0 0 0 0 0 0 0 0 0 0 0 0
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define FIL_NAM "text"
+#define ROWS 20
+#define COLS 30
+
+int main(void) {
+    FILE *text;
+    int ch, num[ROWS][COLS];
+    int *pt = num[0];
+    int ct = 0;
+    if ((text = fopen(FIL_NAM, "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s!", FIL_NAM);
+        exit(EXIT_FAILURE);
+    }
+    while ((ch = getc(text)) != EOF && ct < ROWS * COLS) {
+        if (isdigit(ch)) {
+            pt[ct++] = ch - '0';
+        }
+    }
+
+    if (fclose(text) == EOF) {
+        fprintf(stderr, "Can't close %s.", FIL_NAM);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            switch (num[i][j]) {
+                case 0:
+                    putchar(' ');
+                    break;
+                case 1:
+                    putchar('.');
+                    break;
+                case 3:
+                    putchar('_');
+                    break;
+                case 4:
+                    putchar('=');
+                    break;
+                case 5:
+                    putchar('*');
+                    break;
+                case 6:
+                    putchar('%');
+                    break;
+                case 7:
+                    putchar('$');
+                    break;
+                case 8:
+                    putchar('@');
+                    break;
+                default:
+                    putchar('#');
+            }
+        }
+        putchar('\n');
+    }
+    return 0;
+}
