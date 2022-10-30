6d32d5e2aa6073b52558127a5dd39741d7bdc403
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-10-30 14:11:41 +0800

    Create queue.h

---
+++ MyADT/Queue/queue.h
@@ -0,0 +1,35 @@
+//
+// Created by JavaC on 2022/10/29.
+//
+
+#ifndef TEST_C_QUEUE_H
+#define TEST_C_QUEUE_H
+#include <stdbool.h>
+
+typedef int Item;
+
+#define MAXQUEUE 10
+
+typedef struct node {
+    Item item;
+    struct node * next;
+} Node;
+
+typedef struct queue {
+    Node * front;
+    Node * rear;
+    int items;
+} Queue;
+
+void InitializedQueue(Queue * pq);
+
+bool QueueIsFull(const Queue * pq);
+
+bool QueueIsEmpty(const Queue * pq);
+
+int QueueItemCount(const Queue *pq);
+
+bool EnQueue(Item item, Queue * pq);
+
+bool DeQueue(Item *pitem, Queue * pq);
+#endif //TEST_C_QUEUE_H
