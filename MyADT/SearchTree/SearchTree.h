9bfe4562119e3c27385ccf76a95a010e42664151
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-11-24 22:21:24 +0800

    Create SearchTree.h

---
+++ MyADT/SearchTree/SearchTree.h
@@ -0,0 +1,30 @@
+//
+// Created by Kokomi on 2022/11/24.
+//
+
+#ifndef TREE_SEARCHTREE_H
+#define TREE_SEARCHTREE_H
+struct TreeNode;
+struct ElementType;
+typedef struct ElementType Item;
+typedef struct TreeNode *SearchTree;
+typedef struct TreeNode *Position;
+
+SearchTree MakeEmpty(SearchTree T);
+Position Find(Item item, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(Item item, SearchTree T);
+SearchTree Delete(Item item, SearchTree T);
+Item Retrieve(Position position);
+#endif //TREE_SEARCHTREE_H
+
+struct ElementType {
+    int num;
+};
+
+struct TreeNode {
+    Item item;
+    SearchTree left;
+    SearchTree right;
+};
