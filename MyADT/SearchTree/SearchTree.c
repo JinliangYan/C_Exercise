25d747755670b57e3c24f31a437eaad4458eec8a
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-11-25 09:10:05 +0800

    Update SearchTree.c

--- MyADT/SearchTree/SearchTree.c
+++ MyADT/SearchTree/SearchTree.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "SearchTree.h"
-#define NOT_FOUND NULL
+
 static int compare(Item item1, Item item2);
-static void copy(Item dest, Item source);
+static void copy(Item *dest, Item *source);
 SearchTree MakeEmpty(SearchTree T) {
     if (T != NULL) {
         MakeEmpty(T->left);
@@ -51,7 +51,7 @@ SearchTree Insert(Item item, SearchTree T) {
             fprintf(stderr, "Out of space!\n");
             exit(EXIT_FAILURE);
         } else {
-            copy(T->item, item);
+            copy(&T->item, &item);
             T->right = T->left = NULL;
         }
     }
@@ -65,23 +65,25 @@ SearchTree Insert(Item item, SearchTree T) {
 
 SearchTree Delete(Item item, SearchTree T) {
     Position TmpCell;
-    if (T == NULL)
-        return NOT_FOUND; //defined as NULL
-    if (compare(item, T->item) > 0)
-        T = Delete(item, T->right);
+    if (T == NULL) {
+        fprintf(stderr, "Not found!\n");
+        exit(EXIT_FAILURE);
+    } //defined as NULL
+    else if (compare(item, T->item) > 0)
+        T->right = Delete(item, T->right);
     else if (compare(item, T->item) < 0)
-        T = Delete(item, T->left);
+        T->left = Delete(item, T->left);
     /*其他情况只有找到item，下面把它删除*/
     else if (T->left && T->right) {//两个孩子
         TmpCell = FindMin(T->right);
-        copy(T->item, TmpCell->item);
+        copy(&T->item, &TmpCell->item);
         T->right = Delete(T->item, T->right);
-    } else {
+    } else { //一个孩子
         TmpCell = T;
-        if (T->right) {
-            T = T->right;
-        } else if (T->left) {
+        if (T->right == NULL) { //包括了没有孩子的情况
             T = T->left;
+        } else if (T->left == NULL) {
+            T = T->right;
         }
         free(TmpCell);
     }
@@ -92,9 +94,19 @@ Item Retrieve(Position position) {
     return position->item;
 }
 
+void traverse(SearchTree T, void (*fp)(Item item)) {
+    if (T) {
+        (*fp)(T->item);
+        traverse(T->left, fp);
+        traverse(T->right, fp);
+    } else {
+        return;
+    }
+}
+
 static int compare(Item item1, Item item2) {
     return item1.num - item2.num;
 }
-static void copy(Item dest, Item source) {
-    dest.num = source.num;
+static void copy(Item *dest, Item *source) {
+    dest->num = source->num;
 }
