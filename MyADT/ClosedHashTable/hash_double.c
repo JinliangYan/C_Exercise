43b379e10f493c6a28efa758ff3d98be5bc131d8
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-12-16 22:00:32 +0800

    Update hash_double.c

--- MyADT/ClosedHashTable/hash_double.c
+++ MyADT/ClosedHashTable/hash_double.c
@@ -47,7 +47,7 @@ static HASH_TABLE rehashing(HASH_TABLE hash_table_old) {
     return hash_table_new;
 }
 /*find the next empty spot(to hash_insert) or element's position(to hash_delete)*/
-position find(element_type element, HASH_TABLE hash_table) {
+position hash_find(element_type element, HASH_TABLE hash_table) {
     position current_pos = hash(element, hash_table), i = 0;
     while (hash_table->the_cells[current_pos].info != empty && element != hash_table->the_cells[current_pos].element) {
         current_pos += (++i) * d_hash(element, hash_table);
@@ -86,7 +86,7 @@ HASH_TABLE hash_insert(element_type element, HASH_TABLE hash_table) {
     if (alpha >= 0.5) {
         hash_table = rehashing(hash_table);
     }
-    position current_pos = find(element, hash_table);
+    position current_pos = hash_find(element, hash_table);
     hash_table->the_cells[current_pos].element = element;
     hash_table->the_cells[current_pos].info = legitimate;
     hash_table->size++;
@@ -94,7 +94,7 @@ HASH_TABLE hash_insert(element_type element, HASH_TABLE hash_table) {
 }
 
 HASH_TABLE hash_delete(element_type element, HASH_TABLE hash_table) {
-    position current_pos = find(element, hash_table), i = 0;
+    position current_pos = hash_find(element, hash_table), i = 0;
     while (hash_table->the_cells[current_pos].info != legitimate || element != hash_table->the_cells[current_pos].element) {
         current_pos += (++i) * d_hash(element, hash_table);
         if (current_pos >= hash_table->table_size)
