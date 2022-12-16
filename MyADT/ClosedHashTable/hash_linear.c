6125aa81f6302cd6daf60b6464f8bde4b5b00bf7
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-12-16 16:03:30 +0800

    Update hash_linear.c
    remove static from find()

--- MyADT/ClosedHashTable/hash_linear.c
+++ MyADT/ClosedHashTable/hash_linear.c
@@ -27,7 +27,7 @@ static INDEX hash(element_type element, HASH_TABLE hashTable) {
 }
 
 /*find the next empty spot(to insert) or element's position(to delete)*/
-static position find(element_type element, HASH_TABLE hashTable) {
+position find(element_type element, HASH_TABLE hashTable) {
     position current_pos = hash(element, hashTable), i = 0;
     while (hashTable->the_cells[current_pos].info != empty && element != hashTable->the_cells[current_pos].element) {
         current_pos += ++i;
