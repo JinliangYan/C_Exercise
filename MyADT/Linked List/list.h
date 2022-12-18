c0474444fce9ba4685072bed9a1434b3a74edfc2
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-12-18 22:05:12 +0800

    Ignore the error caused by not found.
    Since this situation happens often, and usually used to judge whther find the item by return NULL.
    I define NOT_FOUND as NULL.

--- MyADT/Linked List/list.h
+++ MyADT/Linked List/list.h
@@ -5,33 +5,33 @@
 #ifndef EXERCISE_LIST_H
 #define EXERCISE_LIST_H
 #define END_POS NULL
+#define NOT_FOUND NULL
 #include <stdbool.h>
 typedef int element_type;
-
 struct node {
     element_type element;
     struct node* next;
 };
 typedef struct node *node_ptr;
-typedef node_ptr LIST;
+typedef node_ptr POLYNOMIAL;
 typedef node_ptr cell;
 typedef node_ptr position;/*header's position is -1*/
 
-LIST list_initialize();
+POLYNOMIAL list_initialize();
 element_type list_retrieve(position p);
-bool list_is_empty(LIST list);
-bool list_is_last(position position, LIST list);
-bool list_is_initialized(LIST list);
-position list_find(element_type element, LIST list);
-void list_delete(element_type element, LIST list);
-position list_find_previous(element_type element, LIST list);
-void list_insert(element_type element, LIST list, position p);
-void delete_list(LIST *list);
-size_t list_get_size(LIST list);
-position list_get_header(LIST list);
-position list_get_first(LIST list);
+bool list_is_empty(POLYNOMIAL list);
+bool list_is_last(position position, POLYNOMIAL list);
+bool list_is_initialized(POLYNOMIAL list);
+position list_find(element_type element, POLYNOMIAL list);
+void list_delete(element_type element, POLYNOMIAL list);
+position list_find_previous(element_type element, POLYNOMIAL list);
+void list_insert(element_type element, POLYNOMIAL list, position p);
+void delete_list(POLYNOMIAL *list);
+size_t list_get_size(POLYNOMIAL list);
+position list_get_header(POLYNOMIAL list);
+position list_get_first(POLYNOMIAL list);
 position list_get_next(position p);
 position list_nget_next(position p, unsigned int n);
-position list_jump_to(LIST list, unsigned int n);
-position list_get_previous(position p, LIST list);
+position list_jump_to(POLYNOMIAL list, unsigned int n);
+position list_get_previous(position p, POLYNOMIAL list);
 #endif //EXERCISE_LIST_H
