2e64c5d776563e9b6c930a764b99e84b66206fb8
严金靓 <52315768+13739388629@users.noreply.github.com>
2022-12-19 10:34:09 +0800

    Optimize the routines of union and overlap.

--- MyADT/Linked List/list_test.c
+++ MyADT/Linked List/list_test.c
@@ -11,7 +11,7 @@ int main(void) {
     L = list_initialize();
     P = list_initialize();
     position position_p = list_get_header(P);
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 20; i += 2) {
         list_insert(i, P, position_p);
         position_p = list_get_next(position_p);
     }
@@ -25,24 +25,33 @@ int main(void) {
 }
 
 void print_lots(LIST L, LIST P) {
-    clock_t start = clock();
     position position_1 = list_get_first(P);
     while (position_1->next != NULL) {
-        printf("%d\n", list_retrieve(list_nget_next(L, list_retrieve(position_1))));
+        printf("%d\n",
+               list_retrieve(list_nget_next(L, list_retrieve(position_1))));
         position_1 = position_1->next;
     }
-    clock_t finish = clock();
-    printf("Run time: %ld\n", finish - start);
 }
 
 LIST get_overlap(LIST list1, LIST list2) {
-//    clock_t start = clock();
+    //    clock_t start = clock();
     LIST overlap_list = list_initialize();
     position p1 = list_get_first(list1);
     position p2 = list_get_first(list2);
-//    position p3 = list_get_first(overlap_list); will cause an insertion error: list_insert(tmp, overlap_list,p3);
     position p3 = list_get_header(overlap_list);
-    element_type tmp;
+    while (p1 != END_POS && p2 != END_POS) {
+        if (list_retrieve(p1) < list_retrieve(p2))
+            p1 = list_get_next(p1);
+        else if (list_retrieve(p1) < list_retrieve(p2))
+            p2 = list_get_next(p2);
+        else {
+            list_insert(list_retrieve(p1), overlap_list, p3);
+            p1 = list_get_next(p1);
+            p2 = list_get_next(p2);
+            p3 = list_get_next(p3);
+        }
+    }
+/*    element_type tmp;
     while (p1 != END_POS) {
         tmp = list_retrieve(p1);
         while (p2 != END_POS && list_retrieve(p2) != tmp)
@@ -52,7 +61,7 @@ LIST get_overlap(LIST list1, LIST list2) {
         list_insert(tmp, overlap_list,p3);
         p1 = list_get_next(p1);
         p3 = list_get_next(p3);
-    }
+    }*/
 //    clock_t finish = clock();
 //    printf("Run time: %ld\n", finish - start);
     return overlap_list;
@@ -65,18 +74,44 @@ LIST get_union(LIST list1, LIST list2) {
     p1 = list_get_first(list1);
     p2 = list_get_first(list2);
     p3 = list_get_header(union_list);
-    while (p1 != END_POS) {
-        if (p2 != NULL && list_retrieve(p2) < list_retrieve(p1)) {
-            /*Ensure that p1 is smaller*/
-            tmp_p = p1;
-            p1 = p2;
-            p2 = tmp_p;
+    while (p1 != END_POS && p2 != END_POS) {
+        if (list_retrieve(p1) > list_retrieve(p2)) {
+            list_insert(list_retrieve(p2), union_list, p3);
+            p2 = list_get_next(p2);
+        } else if (list_retrieve(p1) < list_retrieve(p2)) {
+            list_insert(list_retrieve(p1), union_list, p3);
+            p1 = list_get_next(p1);
+        } else {
+            list_insert(list_retrieve(p1), union_list, p3);
+            p1 = list_get_next(p1);
+            p2 = list_get_next(p2);
         }
-        key = list_retrieve(p1);
-        list_insert(key, union_list, p3);
-        p1 = list_get_next(p1);
-        p2 = list_get_next(p2);
         p3 = list_get_next(p3);
     }
+    /*Flush out remaining list*/
+    while (p1 != END_POS) {
+        list_insert(list_retrieve(p1), union_list, p3);
+        p3 = list_get_next(p3);
+        p1 = list_get_next(p1);
+    }
+    while (p2 != END_POS) {
+        list_insert(list_retrieve(p2), union_list, p3);
+        p3 = list_get_next(p3);
+        p2 = list_get_next(p2);
+    }
+
+//    while (p1 != END_POS) {
+//        if (p2 != NULL && list_retrieve(p2) < list_retrieve(p1)) {
+//            /*Ensure that p1 is smaller*/
+//            tmp_p = p1;
+//            p1 = p2;
+//            p2 = tmp_p;
+//        }
+//        key = list_retrieve(p1);
+//        list_insert(key, union_list, p3);
+//        p1 = list_get_next(p1);
+//        p2 = list_get_next(p2);
+//        p3 = list_get_next(p3);
+//    }
     return union_list;
 }
